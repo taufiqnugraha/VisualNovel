6fa6d774fcdc1cc000eb1d56fc45095092c4cbbb
taufiqnugraha <taufiqnugraha48@gmail.com>
2016-10-05 14:08:26 +0700

    visualNovelProgress

---
+++ Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	string karakterName = "";
+	Sprite characterSprite = null;
+	Sprite[] characterPoses = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ CharacterPashing.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class CharacterPashing : MonoBehaviour {
+	List<DialogLine> lines;
+
+	struct DialogLine{
+		string name;
+		string content;
+		int pose;
+
+		public DialogLine(string n,string c,int p){
+			name = n;
+			content = c;
+			pose = p;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		string file = "Dialog";
+		string sceneNum  = EditorApplication.currentScene;
+		sceneNum = Regex.Replace(sceneNum, "[^0-9]", "" );
+		file += sceneNum;
+		file += ".txt";
+
+		LoadDialog(file);
+	}
+	 
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void LoadDialog(string filename){
+		string file = "Assets/Resources/"+ filename;
+		string line;
+		StreamReader r= new StreamReader(file);
+
+		using (r){
+			do{
+
+				line = r.ReadLine();
+				if(line != null) {
+					string[] line_values = line.Split(',');
+					DialogLine line_entry = new DialogLine(line_values[0],line_values[1], int.Parse (line_values[2]));
+					lines.Add(line_entry);
+				}
+			}
+			while(line != null);
+			r.Close();
+		
+		}
+	}
+}
